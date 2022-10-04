7fab577e79f6e4d5e83655a8616bbaa1e326e469
adrixo <adrianvalrom@gmail.com>
2022-10-04 16:18:42 +0200

    refactor: invert if and group conditions in easier conditions

--- src/main/java/com/gildedrose/characterization/GildedRose.java
+++ src/main/java/com/gildedrose/characterization/GildedRose.java
@@ -11,33 +11,30 @@ class GildedRose {
         for (ItemDecorator item : items) {
             item.reduceSellIn();
 
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                item.decreaseQuality();
-            } else {
+            if (item.name.equals("Aged Brie")) {
+                item.increaseQuality();
+            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 item.increaseQuality();
 
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 10) {
-                        item.increaseQuality();
-                    }
-
-                    if (item.sellIn < 5) {
-                        item.increaseQuality();
-                    }
+                if (item.sellIn < 10) {
+                    item.increaseQuality();
                 }
+
+                if (item.sellIn < 5) {
+                    item.increaseQuality();
+                }
+            } else {
+                item.decreaseQuality();
             }
 
 
             if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        item.decreaseQuality();
-                    } else {
-                        item.dropQualityToZero();
-                    }
-                } else {
+                if (item.name.equals("Aged Brie")) {
                     item.increaseQuality();
+                } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    item.dropQualityToZero();
+                } else {
+                    item.decreaseQuality();
                 }
             }
         }
