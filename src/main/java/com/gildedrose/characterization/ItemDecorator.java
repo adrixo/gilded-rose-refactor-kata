196738131671e01d6379a90cf09ed73c4ba18f9c
adrixo <adrianvalrom@gmail.com>
2022-10-04 17:15:52 +0200

    comment: identify items properties

--- src/main/java/com/gildedrose/characterization/ItemDecorator.java
+++ src/main/java/com/gildedrose/characterization/ItemDecorator.java
@@ -7,30 +7,38 @@ public class ItemDecorator extends Item {
 
     void updateQuality() {
         if (name.equals("Sulfuras, Hand of Ragnaros")){
+            // Never degradates properties
             return;
         } else if (name.equals("Aged Brie")) {
+            // Aged properties
             increaseQuality();
             if (sellIn < 0) {
+                // Aged properties with threshold
                 increaseQuality();
             }
         } else if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (sellIn < 0) {
+                // Extreme degradation with threshold
                 dropQualityToZero();
                 return;
             }
-
+            // Aged properties with threshold
             increaseQuality();
 
             if (sellIn < 10) {
+                // Aged properties with threshold
                 increaseQuality();
             }
 
             if (sellIn < 5) {
+                // Aged properties with threshold
                 increaseQuality();
             }
         } else {
+            // degradation property
             decreaseQuality();
             if (sellIn < 0) {
+                // degradation property with threshold
                 decreaseQuality();
             }
         }
@@ -38,7 +46,9 @@ public class ItemDecorator extends Item {
 
     void reduceSellIn() {
         if (name.equals("Sulfuras, Hand of Ragnaros"))
+            // Never pass properties
             return;
+        // Time pass properties
         sellIn--;
     }
 
