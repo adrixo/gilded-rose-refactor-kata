6f6b16d7ec3f42f30e0085c57d007185cae48ac1
adrixo <adrianvalrom@gmail.com>
2022-10-04 14:25:41 +0200

    refactor: move sellIn reduction to top of updateQuality function preserve same code logic

--- src/main/java/com/gildedrose/characterization/GildedRose.java
+++ src/main/java/com/gildedrose/characterization/GildedRose.java
@@ -9,6 +9,10 @@ class GildedRose {
 
     public void updateQuality() {
         for (ItemDecorator item : items) {
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.reduceSellIn();
+            }
+
             if (!item.name.equals("Aged Brie")
                     && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
@@ -18,19 +22,16 @@ class GildedRose {
                 item.increaseQuality();
 
                 if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11) {
+                    if (item.sellIn < 10) {
                         item.increaseQuality();
                     }
 
-                    if (item.sellIn < 6) {
+                    if (item.sellIn < 5) {
                         item.increaseQuality();
                     }
                 }
             }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.reduceSellIn();
-            }
 
             if (item.sellIn < 0) {
                 if (!item.name.equals("Aged Brie")) {
